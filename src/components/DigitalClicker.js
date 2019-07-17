495bb76b73dfecad4a13227cc6fa47378e24f5e7
Your Name <you@example.com>
2019-07-17 09:13:29 -0400

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,28 @@
 // Code DigitalClicker Component Here
+import React from 'react';
+
+class DigitalClicker extends React.Component{
+  constructor(){
+    super();
+
+    this.state = {
+      timesClicked: 0
+    }
+  }
+  updateTimesClickedValue = () =>{
+    this.setState({
+      timesClicked: this.state.timesClicked + 1
+    })
+  }
+
+  render(){
+    return(
+      <div>
+      <label>timesClicked:</label>
+
+      <button onClick = {this.updateTimesClickedValue}>{this.state.timesClicked}</button>
+      </div>
+    )
+  }
+}
+export default DigitalClicker
